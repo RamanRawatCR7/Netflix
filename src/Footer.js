b5e20663ed8960ea56f38dd5561b87033b0c12ef
Raman_Rawat <55266325+RamanRawatCR7@users.noreply.github.com>
2022-07-13 13:09:31 +0530

    footer

---
+++ src/Footer.js
@@ -0,0 +1,99 @@
+import React from "react";
+import "./Footer.css";
+function Footer() {
+  return (
+    <div class="footer">
+      <div className="grey_border" />
+      <div class="footercon">
+        <div class="flex1">
+          <p>Questions? Call <a href="">000-800-040-1843</a></p>
+        </div>
+        <ul class="list1">
+          <li>
+            <a href="">FAQ</a>
+          </li>
+          <li>
+            <a href="">Investor Relation</a>
+          </li>
+          <li>
+            <a href="">Privacy</a>
+          </li>
+          <li>
+            <a href="">Speed Test</a>
+          </li>
+        </ul>
+        <ul class="list1">
+          <li>
+            <a href="">Help Centre</a>
+          </li>
+          <li>
+            <a href="">Jobs</a>
+          </li>
+          <li>
+            <a href="">Cookie Preferences</a>
+          </li>
+          <li>
+            <a href="">Legal Notices</a>
+          </li>
+        </ul>
+        <ul class="list1">
+          <li>
+            <a href="">Account</a>
+          </li>
+          <li>
+            <a href="">Ways to Watch</a>
+          </li>
+          <li>
+            <a href="">Corporate Information</a>
+          </li>
+          <li>
+            <a href="">Only on Netflix</a>
+          </li>
+        </ul>
+        <ul class="list1">
+          <li>
+            <a href="">Media Centre</a>
+          </li>
+          <li>
+            <a href="">Terms of Use</a>
+          </li>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+          <li>
+            <a>#</a>
+          </li>
+        </ul>
+        <div class="end">
+          <p>Netflix India</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
+
+{
+  /* <footer>
+<div className='footer_background'>
+  <div className='faq'>
+    <p className='col'>FAQ</p>
+    <p className='col'>Help Centre</p>
+    <p className='col'>Account</p>
+    <p className='col'>Media Centre</p><br />
+    <p className='col'>Investor Relations</p>
+    <p className='col'>Jobs</p>
+    <p className='col'>Ways to Watch</p>
+    <p className='col'>Terms of Use</p><br />
+    <p className='col'>Privacy</p>
+    <p className='col'>Cookie Preferences</p>
+    <p className='col'>Corporate information</p>
+    <p className='col'>Contact Us</p><br />
+    <p className='col'>Speed Test</p>
+    <p className='col'>Legal Notices</p>
+    <p className='col'>Only on Netlix</p>
+  </div>
+</div>
+</footer> */
+}
---
+++ src/firebaseconfig.js
@@ -0,0 +1,13 @@
+export const firebaseconfig = {
+    apiKey: "AIzaSyBygDwhs8F8vJa-FDSj7bJ1u_AZUhGAkKU",
+    authDomain: "netflix-clone-899b8.firebaseapp.com",
+    projectId: "netflix-clone-899b8",
+    storageBucket: "netflix-clone-899b8.appspot.com",
+    messagingSenderId: "104691059483",
+    appId: "1:104691059483:web:167fc91573774983d161be",
+    measurementId: "G-PJR0K6W9BF"
+  };
+
+export const stripeconfig = {
+  publicKey: "pk_test_51L8gqJSH8XpbCPj0JehAoBocjgYe5NBBW5RekaFCXHiFedH23KYdap1e9VRpgj9pgJLPtZbEMWftPyoZik5oVUf100KOBv5yFA"
+}
